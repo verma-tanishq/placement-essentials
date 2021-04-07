c67b30da29a7038cdd2e91a8f13b9ac0541235df
Tanishq Verma <70758368+verma-tanishq@users.noreply.github.com>
2021-04-07 09:12:56 +0530

    Update prefix_infix_postfix_evaluation.cpp

--- Stack/prefix_infix_postfix_evaluation.cpp
+++ Stack/prefix_infix_postfix_evaluation.cpp
@@ -1,52 +1,52 @@
-// #include<iostream>
-// #include<math.h>
-// #include<stack>
-// using namespace std;
+#include<iostream>
+#include<math.h>
+#include<stack>
+using namespace std;
 
-// int prefixevaluation(string str){
-//     stack <int> st;
-//     for(int i=str.length()-1; i>=0; i--){
-//         if(str[i]>='0' && str[i]<='9'){
-//             st.push(str[i]-'0');
-//         }
+int prefixevaluation(string str){
+    stack <int> st;
+    for(int i=str.length()-1; i>=0; i--){
+        if(str[i]>='0' && str[i]<='9'){
+            st.push(str[i]-'0');
+        }
 
-//         else{
-//             int opr1 = st.top();
-//             st.pop();
-//             int opr2 = st.top();
-//             st.pop();
+        else{
+            int opr1 = st.top();
+            st.pop();
+            int opr2 = st.top();
+            st.pop();
 
-//             switch (str[i])
-//             {
-//             case '+':
-//                 st.push(opr1+opr2);
-//                 break;
+            switch (str[i])
+            {
+            case '+':
+                st.push(opr1+opr2);
+                break;
             
-//             case '-':
-//                 st.push(opr1-opr2);
-//                 break;
+            case '-':
+                st.push(opr1-opr2);
+                break;
             
-//             case '*':
-//                 st.push(opr1*opr2);
-//                 break;
+            case '*':
+                st.push(opr1*opr2);
+                break;
             
-//             case '/':
-//                 st.push(opr1/opr2);
-//                 break;
+            case '/':
+                st.push(opr1/opr2);
+                break;
             
-//             case '^':
-//                 st.push(pow(opr1,opr2));
-//                 break;
-//             }
-//         }
-//     }
-//     return st.top();
-// }
+            case '^':
+                st.push(pow(opr1,opr2));
+                break;
+            }
+        }
+    }
+    return st.top();
+}
 
-// int main(){
-//     cout<<prefixevaluation("-+7*45+20")<<endl;
-//     return 0;
-// }
+int main(){
+    cout<<prefixevaluation("-+7*45+20")<<endl;
+    return 0;
+}
 
 #include<bits/stdc++.h>
 using namespace std;
