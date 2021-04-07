fe4eb07798f79302a1639112b163b874ff1a7fc7
Tanishq Verma <70758368+verma-tanishq@users.noreply.github.com>
2021-04-07 09:18:01 +0530

    Update infix_to_postfix_allcases.cpp

--- Stack/infix_to_postfix_allcases.cpp
+++ Stack/infix_to_postfix_allcases.cpp
@@ -1,61 +1,61 @@
-// #include<bits/stdc++.h>
-// using namespace std;
+#include<bits/stdc++.h>
+using namespace std;
 
-// int precedent(char c){
-//     if(c=='^'){
-//         return 3;
-//     }
-//     else if(c=='*' || c=='/'){
-//         return 2;
-//     }
+int precedent(char c){
+    if(c=='^'){
+        return 3;
+    }
+    else if(c=='*' || c=='/'){
+        return 2;
+    }
 
-//     else if(c=='+' || c=='-'){
-//         return 1;
-//     }
+    else if(c=='+' || c=='-'){
+        return 1;
+    }
 
-//     else{
-//         return -1;
-//     }
-// }
+    else{
+        return -1;
+    }
+}
 
-// string infixToPostfix(string str){
-//     stack<char> st;
-//     string res;
-//     for(int i=0; i<str.length(); i++){
-//         if(str[i]>='a' && str[i]<='z' || str[i]>='A' && str[i]<='Z'){
-//             res+=str[i];
-//         }
-//         else if(str[i]=='('){
-//             st.push(str[i]);
-//         }
-//         else if(str[i]==')'){
-//             while(!st.empty() && st.top()!='('){
-//                 res+=st.top();
-//                 st.pop();
-//             }
-//             if(!st.empty()){
-//                 st.pop();
-//             }
-//         }
-//         else{
-//             while(!st.empty() && precedent(st.top())>precedent(str[i])){
-//                 res+=st.top();
-//                 st.pop();
-//             }
-//             st.push(str[i]);
-//         }
-//     }
-//     while(!st.empty()){
-//         res+=st.top();
-//         st.pop();
-//     }
-//     return res;
-// }
+string infixToPostfix(string str){
+    stack<char> st;
+    string res;
+    for(int i=0; i<str.length(); i++){
+        if(str[i]>='a' && str[i]<='z' || str[i]>='A' && str[i]<='Z'){
+            res+=str[i];
+        }
+        else if(str[i]=='('){
+            st.push(str[i]);
+        }
+        else if(str[i]==')'){
+            while(!st.empty() && st.top()!='('){
+                res+=st.top();
+                st.pop();
+            }
+            if(!st.empty()){
+                st.pop();
+            }
+        }
+        else{
+            while(!st.empty() && precedent(st.top())>precedent(str[i])){
+                res+=st.top();
+                st.pop();
+            }
+            st.push(str[i]);
+        }
+    }
+    while(!st.empty()){
+        res+=st.top();
+        st.pop();
+    }
+    return res;
+}
 
-// int main(){
-//     cout<<infixToPostfix("(a-b/c)*(a/k-l)")<<endl;
-//     return 0;
-// }
+int main(){
+    cout<<infixToPostfix("(a-b/c)*(a/k-l)")<<endl;
+    return 0;
+}
 
 // abc/-ak/l-*
 
