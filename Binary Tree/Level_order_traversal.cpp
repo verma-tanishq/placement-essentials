1234f3fc82d61ea9023b4550d0ef55ec6c8aec80
Tanishq Verma <70758368+verma-tanishq@users.noreply.github.com>
2021-04-11 13:58:10 +0530

    Update Level_order_traversal.cpp

--- Binary Tree/Level_order_traversal.cpp
+++ Binary Tree/Level_order_traversal.cpp
@@ -37,6 +37,36 @@ void printLevel(Node* root){
         }
     }
 }
+int sumAtK(Node* root, int k){
+    if(root==NULL){
+        return -1;
+    }
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+    int sum=0;
+    int level = 0;
+    while(!q.empty()){
+        Node* node = q.front();
+        q.pop();
+        if(node!=NULL){
+            if(level==k){
+                sum+=node->data;
+            }
+            if(node->left){
+                q.push(node->left);
+            }
+            if(node->right){
+                q.push(node->right);
+            }
+        }
+        else if(!q.empty()){
+            q.push(NULL);
+            level++;
+        }
+    }
+    return sum;
+}
 
 
 int main(){
@@ -48,5 +78,6 @@ int main(){
     root->right->left = new Node(6);
     root->right->right = new Node(7);
     printLevel(root);
+    cout<<sumAtK(root,2);  //output =>22 (4+5+6+7)
     return 0;
 }
