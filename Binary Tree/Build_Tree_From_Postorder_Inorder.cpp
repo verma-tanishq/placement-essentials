5273fa6f938114da125db63270a58a19c930d923
Tanishq Verma <70758368+verma-tanishq@users.noreply.github.com>
2021-05-16 09:22:11 +0530

    Update Build_Tree_From_Postorder_Inorder.cpp

--- Binary Tree/Build_Tree_From_Postorder_Inorder.cpp
+++ Binary Tree/Build_Tree_From_Postorder_Inorder.cpp
@@ -1,3 +1,16 @@
+// Let us see the process of constructing tree from in[] = {4, 8, 2, 5, 1, 6, 3, 7} and post[] = {8, 4, 5, 2, 6, 7, 3, 1} 
+// 1) We first find the last node in post[]. The last node is “1”, we know this value is root as root always appear in the end of postorder traversal.
+// 2) We search “1” in in[] to find left and right subtrees of root. Everything on left of “1” in in[] is in left subtree and everything on right is in right subtree. 
+
+//          1
+//        /    \
+// [4, 8, 2, 5]   [6, 3, 7]
+// 3) We recur the above process for following two. 
+// ….b) Recur for in[] = {6, 3, 7} and post[] = {6, 7, 3} 
+// …….Make the created tree as right child of root. 
+// ….a) Recur for in[] = {4, 8, 2, 5} and post[] = {8, 4, 5, 2}. 
+// …….Make the created tree as left child of root.
+
 #include<iostream>
 using namespace std;
 
