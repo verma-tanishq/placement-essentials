c06c8e1ffb0c3c449b942fc51afa9bfc2f5a18e0
Tanishq Verma <70758368+verma-tanishq@users.noreply.github.com>
2021-06-16 23:29:45 +0530

    Update balancedParenthesis.cpp

--- Stack/balancedParenthesis.cpp
+++ Stack/balancedParenthesis.cpp
@@ -1,49 +1,95 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool isValid(string str){
-    stack<char> st;
-    bool ans=true;
-    for(int i=0; i<str.length(); i++){
-        if(str[i]=='{' or str[i]=='[' or str[i]=='('){
-            st.push(str[i]);
-        }
-        else if(!st.empty() and str[i]=='}'){
-            if(st.top()=='{'){
-                st.pop();
-            }
-            else{
-                ans=false;
-                break;
-            }
-        }
-        else if(!st.empty() and str[i]==']'){
-            if(st.top()=='['){
-                st.pop();
-            }
-            else{
-                ans=false;
-                break;
-            }
-        }
-        else if(!st.empty() and str[i]==')'){
-            if(st.top()=='('){
-                st.pop();
-            }
-            else{
-                ans=false;
-                break;
-            }
-        }
+// bool isValid(string str){
+//     stack<char> st;
+//     bool ans=true;
+//     for(int i=0; i<str.length(); i++){
+//         if(str[i]=='{' or str[i]=='[' or str[i]=='('){
+//             st.push(str[i]);
+//         }
+//         else if(!st.empty() and str[i]=='}'){
+//             if(st.top()=='{'){
+//                 st.pop();
+//             }
+//             else{
+//                 ans=false;
+//                 break;
+//             }
+//         }
+//         else if(!st.empty() and str[i]==']'){
+//             if(st.top()=='['){
+//                 st.pop();
+//             }
+//             else{
+//                 ans=false;
+//                 break;
+//             }
+//         }
+//         else if(!st.empty() and str[i]==')'){
+//             if(st.top()=='('){
+//                 st.pop();
+//             }
+//             else{
+//                 ans=false;
+//                 break;
+//             }
+//         }
+//     }
+//     if(!st.empty()){
+//         return false;
+//     }
+//     return ans;
+// }
+class Solution
+{
+    public:
+    //Function to check if opening and closing brackets are of same type.
+    bool cmp(char b,char c)
+    {
+        if(b=='{' && c=='}')
+            return true;
+        else if(b=='[' && c==']')
+            return true;
+        else if(b=='(' && c==')')
+            return true;
+         return false;
     }
-    if(!st.empty()){
-        return false;
+    
+    //Function to check if brackets are balanced or not.
+    bool ispar(string x)
+    {
+        stack<char> s;
+        
+        //iterating over the string.
+        for(int i=0;i<x.size();i++)
+        {
+            //if opening bracket is encountered, we push it into stack.
+            if(x[i]=='[' || x[i]=='{' || x[i]=='(')
+                s.push(x[i]);
+            
+            //if closing bracket is encountered, we compare it with top of stack.
+            else if(x[i]==']' || x[i]=='}' || x[i]==')')
+            {
+                //if top of stack has opening bracket of different
+                //type, we return false.
+                if(s.empty() || !cmp(s.top(),x[i])) 
+                    return false;
+                //else we pop the top element from stack.
+                else
+                    s.pop();
+            }
+        }
+        //if stack becomes empty, we return true else false.
+        if(s.empty())
+            return true;
+        else
+            return false;
     }
-    return ans;
-}
+};
 
 int main(){
-    if(isValid("{[()]}")){
+    if(ispar("{[()]}")){
         cout<<"Valid"<<endl;
     }
     else{
