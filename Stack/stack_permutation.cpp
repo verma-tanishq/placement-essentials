f05846d32aca6adac2f055b5c48c69c6e5672c29
Tanishq Verma <70758368+verma-tanishq@users.noreply.github.com>
2021-06-19 10:42:02 +0530

    Update stack_permutation.cpp

--- Stack/stack_permutation.cpp
+++ Stack/stack_permutation.cpp
@@ -1,56 +1,66 @@
+// Given two arrays, check if one array is
+// stack permutation of other.
 #include<bits/stdc++.h>
 using namespace std;
 
-bool checkStackPermutation(int ip[], int ot[], int n){
-    stack<int> st;
-    queue<int> input;
-    queue<int> output;
+// function to check if input queue is
+// permutable to output queue
+bool checkStackPermutation(int ip[], int op[], int n)
+{
+	// Input queue
+	queue<int> input;
+	for (int i=0;i<n;i++)
+		input.push(ip[i]);
 
-    for(int i=0; i<n; i++){
-        input.push(ip[i]);
-    }
+	// output queue
+	queue<int> output;
+	for (int i=0;i<n;i++)
+		output.push(op[i]);
 
-    for(int i=0; i<n; i++){
-        output.push(ot[i]);
-    }
+	// stack to be used for permutation
+	stack <int> tempStack;
+	while (!input.empty())
+	{
+		int ele = input.front();
+		input.pop();
+		if (ele == output.front())
+		{
+			output.pop();
+			while (!tempStack.empty())
+			{
+				if (tempStack.top() == output.front())
+				{
+					tempStack.pop();
+					output.pop();
+				}
+				else
+					break;
+			}
+		}
+		else
+			tempStack.push(ele);
+	}
 
-    while(!input.empty()){
-        int ele = input.front();
-        input.pop();
-
-        if(ele==output.front()){
-            output.pop();
-            while(!st.empty()){
-                if(st.top()==output.front()){
-                    st.pop();
-                    output.pop();
-                }
-                else{
-                    break;
-                }
-            }
-        }
-        else{
-            st.push(ele);
-        }
-    }
-    return (input.empty() && st.empty());
-    
+	// If after processing, both input queue and
+	// stack are empty then the input queue is
+	// permutable otherwise not.
+	return (input.empty()&&tempStack.empty());
 }
 
+// Driver program to test above function
 int main()
 {
-    // Input Queue
-    int input[] = {1, 2, 3};
-  
-    // Output Queue
-    int output[] = {2, 1, 3};
-  
-    int n = 3;
-  
-    if (checkStackPermutation(input, output, n))
-        cout << "Yes";
-    else
-        cout << "Not Possible";
-    return 0;
+	// Input Queue
+	int input[] = {1, 2, 3};
+
+	// Output Queue
+	int output[] = {2, 1, 3};
+
+	int n = 3;
+
+	if (checkStackPermutation(input, output, n))
+		cout << "Yes";
+	else
+		cout << "Not Possible";
+	return 0;
 }
