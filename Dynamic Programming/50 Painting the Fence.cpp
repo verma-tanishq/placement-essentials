57397cfd6b2420e96df47ddc421b2767bad6dc97
Tanishq Verma <70758368+verma-tanishq@users.noreply.github.com>
2021-07-09 22:51:59 +0530

    Create 50 Painting the Fence.cpp

---
+++ Dynamic Programming/50 Painting the Fence.cpp
@@ -0,0 +1,39 @@
+// C++ program for Painting Fence Algorithm
+// optimised version
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Returns count of ways to color k posts
+long countWays(int n, int k)
+{
+	long dp[n + 1];
+	memset(dp, 0, sizeof(dp));
+	long long mod = 1000000007;
+
+	dp[1] = k;
+	dp[2] = k * k;
+
+	for (int i = 3; i <= n; i++) {
+		dp[i] = ((k - 1) * (dp[i - 1] + dp[i - 2])) % mod;
+	}
+  /*
+  long long same=0, diff=k, total=diff+sum;
+  for(int i=2; i<=n; i++){
+            same = diff;
+            diff = total*(k-1);
+            diff = diff%mod;
+            total = (diff+same)%mod;
+        }
+  */
+
+	return dp[n];
+}
+
+// Driver code
+int main()
+{
+	int n = 3, k = 2;
+	cout << countWays(n, k) << endl;
+	return 0;
+}
