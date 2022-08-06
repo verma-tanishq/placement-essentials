c16a6ac01f18b667b9195d0c68e66256aacdc161
Tanishq Verma <70758368+verma-tanishq@users.noreply.github.com>
2022-08-06 22:11:03 +0530

    Update Height_N_Diameter_Binary_Tree.cpp

--- Binary Tree/Height_N_Diameter_Binary_Tree.cpp
+++ Binary Tree/Height_N_Diameter_Binary_Tree.cpp
@@ -1,52 +1,84 @@
-#include<iostream>
-#include<algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Node{
     public:
     int data;
-    Node* right;
     Node* left;
-    Node(int val){
-        data=val;
-        right=NULL;
-        left=NULL;
+    Node* right;
+
+    Node(int value){
+        this->data = value;
+        left = NULL;
+        right = NULL;
     }
 };
 
-int calcHeight(Node* root){
+int heightOfBinaryTree(Node* root){
     if(root==NULL){
         return 0;
     }
-    int leftH = calcHeight(root->left);
-    int rightH = calcHeight(root->right);
-
-    return max(leftH, rightH) + 1;
+    int leftNode = heightOfBinaryTree(root->left);
+    int rightNode = heightOfBinaryTree(root->right);
+    return max(leftNode,rightNode)+1;
 }
 
-int calcDiameter(Node* root){
+int DiameterOfBinaryTree(Node* root){
     if(root==NULL){
         return 0;
     }
-    int leftH = calcHeight(root->left);
-    int rightH = calcHeight(root->right);
-    int currDia = leftH+ rightH+1;
+    int op1 = DiameterOfBinaryTree(root->left);
+    int op2 = DiameterOfBinaryTree(root->right);
+    int op3 = heightOfBinaryTree(root->left)+heightOfBinaryTree(root->right)+1;
 
-    int lDia = calcDiameter(root->left);
-    int rDia = calcDiameter(root->right);
+    return max(op1,max(op2,op3));
+}
 
-    return max(currDia, max(lDia,rDia));
+
+// More Optimize approach TC = O(n)
+pair<int,int> diameterFast(Node* root){
+    pair<int,int> p = make_pair(0,0);
+    if(root==NULL){
+        return p;
+    }
+    pair<int,int> left = diameterFast(root->left);
+    pair<int,int> right = diameterFast(root->right);
+
+    int op1 = left.first;
+    int op2 = right.first;
+    int op3 = left.second+right.second+1;
+
+    pair<int,int> ans;
+    ans.first = max(op1,max(op2,op3));
+    ans.second = max(left.second, right.second)+1;
+    return ans;
+}
+
+int diameter(Node* root){
+    return diameterFast(root).first;
 }
 
 int main(){
+    // Node* root = new Node(1);
+    // root->left  = new Node(12);
+    // root->right = new Node(13);
+   
+    // root->right->left   = new Node(14);
+    // root->right->right  = new Node(15);
+   
+    // root->right->left->left   = new Node(21);
+    // root->right->left->right  = new Node(22);
+    // root->right->right->left  = new Node(23);
+    // root->right->right->right = new Node(24);
+
     Node* root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
     root->left->left = new Node(4);
     root->left->right = new Node(5);
-    root->right->left = new Node(6);
-    root->right->right = new Node(7);
-    cout<<calcHeight(root)<<endl;
-    cout<<calcDiameter(root)<<endl;
+
+    //cout<<"Diameter of Binary Tree is: "<<DiameterOfBinaryTree(root);
+    cout<<"Diameter of Binary Tree is: "<<diameter(root);
+    
     return 0;
 }
