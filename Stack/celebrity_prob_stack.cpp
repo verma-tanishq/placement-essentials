d4282f758d7f8371b45bd9e8367c5f5fc2f16838
Tanishq Verma <70758368+verma-tanishq@users.noreply.github.com>
2022-08-30 11:36:10 +0530

    Update celebrity_prob_stack.cpp

--- Stack/celebrity_prob_stack.cpp
+++ Stack/celebrity_prob_stack.cpp
@@ -1,105 +1,90 @@
+// https://practice.geeksforgeeks.org/problems/the-celebrity-problem/1
+
+//{ Driver Code Starts
+//Initial template for C++
+
 #include<bits/stdc++.h>
 using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
 class Solution 
-{
+{   
+    bool knows(vector<vector<int> >& M, int n, int a, int b){
+        if(M[a][b]==1){
+            return true;
+        }
+        return false;
+    }
     public:
     //Function to find if there is a celebrity in the party or not.
-    int celebrity(vector<vector<int> >& c, int n) 
+    int celebrity(vector<vector<int> >& M, int n) 
     {
         // code here 
-        // int a = 0;
-        // int b = n - 1;
+        stack<int> s;
+        int a,b;
         
-        // //we keep moving till the a<b. 
-        // while (a < b) {
-        //     if (M[a][b])
-                
-        //         a++;
-        //     else
-        //         b--;
-        //     //cout<<M[a][b]<<endl;
-        // }
+        for(int i=0; i<n; i++){
+            s.push(i);
+        }
         
-        // //checking if a is actually a celebrity or not.
-        // for (int i = 0; i < n; i++)
-        // {
-        //     //if any person doesn't know a or a knows any person, we return -1. 
-        //     if ( (i != a) && (M[a][i] || !M[i][a]) )
-        //         return -1;
-        // }
-        // //if we reach here a is celebrity so we retun a.
-        // return a;
-
-       stack<int> s;
-       //push all the member from 0 to n-1 into the stack
-       for(int i=0;i<n;++i)
-           s.push(i);
-           
-       while(s.size()!=1)
-       {
-           int x=s.top();
-           s.pop();
-           int y=s.top();
-           s.pop();
-           
-           // if x knows y then x cannot be a celebrity.
-           if(c[x][y]==1)
-              s.push(y);
-              
-           // if x doesnt know y then y cannot be celebrity.       
-           else
-              s.push(x);
-       }
-       
-       // ans variable stores the possible index of celebrity
-       int ans=s.top();
-       int i,row=ans,col=ans;
-       
-       //checking the row of the possible answer
-       for(i=0;i<n;++i)
-          if(c[row][i]==1)
-              break;
-      // this means that our possble celebrity does not know somebody
-      // and hence ans cannot be a celebrity
-      if(i!=n)
-         return -1;
-     
-      //checking the column of the possible answer    
-       for(i=0;i<n;++i)
-           {
-               if(i==ans)
-                   continue;
-                   
-               else
-                 {
-                     if(c[i][col]==0)
-                        break;
-                 }
-           }
-        // this means that somebody does not know our possible celebrity
-        // and hence ans cannot be a celebrity   
-        if(i!=n)
+        while(s.size()>1){
+            a = s.top();
+            s.pop();
+            b = s.top();
+            s.pop();
+            if(knows(M,n,a,b)){
+                s.push(b);
+            }
+            else{
+                s.push(a);
+            }
+        }
+        int top = s.top();
+        int rowCount = 0;
+        int colCount = 0;
+        for(int i=0; i<n; i++){
+            if(M[top][i]==0){
+                rowCount++;
+            }
+            if(M[i][top]==1){
+                colCount++;
+            }
+        }
+        
+        if(colCount==n-1 && rowCount==n){
+            return top;
+        }
+        else{
             return -1;
-            
-      // this means the answer found earlier is the celebrity      
-        return ans;    
+        }
+        
     }
 };
 
-int main(){
+//{ Driver Code Starts.
+
+int main()
+{
     int t;
     cin>>t;
-    while(t--){
+    while(t--)
+    {
         int n;
         cin>>n;
-        vector<vector<int>>M(n, vector<int>(n,0));
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
+        vector<vector<int> > M( n , vector<int> (n, 0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
                 cin>>M[i][j];
             }
         }
         Solution ob;
         cout<<ob.celebrity(M,n)<<endl;
+
     }
-    return 0;
 }
+
+// } Driver Code Ends
