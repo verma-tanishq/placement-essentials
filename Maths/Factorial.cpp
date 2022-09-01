8552edf54a22231596a51b32eb8aed4c637cdbf8
Tanishq Verma <70758368+verma-tanishq@users.noreply.github.com>
2022-09-01 22:37:05 +0530

    Create Factorial.cpp

---
+++ Maths/Factorial.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    long factorial = 1.0;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    if (n < 0)
+        cout << "Error! Factorial of a negative number doesn't exist.";
+    else {
+        for(int i = 1; i <= n; ++i) {
+            factorial *= i;
+        }
+        cout << "Factorial of " << n << " = " << factorial;    
+    }
+
+    return 0;
+}
+// Recursive Approach
+#include<iostream>
+using namespace std;
+
+int factorial(int n);
+
+int main() {
+
+  int n;
+
+  cout << "Enter a positive integer: ";
+  cin >> n;
+
+  cout << "Factorial of " << n << " = " << factorial(n);
+
+  return 0;
+}
+
+int factorial(int n) {
+  if(n > 1)
+    return n * factorial(n - 1);
+  else
+    return 1;
+}
